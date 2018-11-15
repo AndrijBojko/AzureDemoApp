b3279d5754f9680bbc3b02461614a03154b5b1d2
Andrii Boiko <andrij.boiko@gmail.com>
2018-11-15 13:14:53 +0200

    initial commit

---
+++ AzureAppExapmle/Controllers/BlobsController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace AzureAppExapmle.Controllers
+{
+    public class BlobsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        private CloudBlobContainer GetCloudBlobContainer()
+        {
+            string connString = "";// use Access Key on azure portal to get conn string
+            string containerName = "pictures";
+
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(connString);
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+            CloudBlobContainer container = blobClient.GetContainerReference(containerName);
+            return container;
+        }
+
+        public string UploadBlob()
+        {
+            CloudBlobContainer container = GetCloudBlobContainer();
+            CloudBlockBlob blob = container.GetBlockBlobReference("sample");
+            using (var fileStream = System.IO.File.OpenRead(@"d:\sample.txt"))
+            {
+                blob.UploadFromStreamAsync(fileStream).GetAwaiter().GetResult();
+            }
+            return "success!";
+        }
+
+        public string DownloadBlob()
+        {
+            CloudBlobContainer container = GetCloudBlobContainer();
+            CloudBlockBlob blob = container.GetBlockBlobReference("sample");
+            using (var fileStream = System.IO.File.OpenWrite(@"d:\downloadedBlob.txt"))
+            {
+                blob.DownloadToStreamAsync(fileStream).GetAwaiter().GetResult();
+            }
+            return "success!";
+        }
+
+        public string DeleteBlob()
+        {
+            CloudBlobContainer container = GetCloudBlobContainer();
+            CloudBlockBlob blob = container.GetBlockBlobReference("sample");
+            blob.DeleteAsync().GetAwaiter().GetResult();
+            return "success!";
+        }
+
+        public ActionResult ListBlobs()
+        {
+            CloudBlobContainer container = GetCloudBlobContainer();
+            List<string> blobs = new List<string>();
+
+            BlobContinuationToken continuationToken = null;
+            do
+            {
+                var response = container.ListBlobsSegmentedAsync(continuationToken).GetAwaiter().GetResult();
+                continuationToken = response.ContinuationToken;
+                var tempres= response.Results;
+
+                foreach (var item in tempres)
+                {
+                    CloudBlockBlob blob = (CloudBlockBlob)item;
+                    blobs.Add(blob.Name);
+                }
+            }
+            while (continuationToken != null);
+
+            return View(blobs);
+        }
+    }
+}
---
+++ AzureAppExapmle/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using AzureAppExapmle.Models;
+
+namespace AzureAppExapmle.Controllers
+{
+    public class HomeController : Controller
+    {
+        ExampleDBContext _context = new ExampleDBContext();
+
+        public IActionResult Index()
+        {
+            return View(_context.Product.FirstOrDefault());
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
