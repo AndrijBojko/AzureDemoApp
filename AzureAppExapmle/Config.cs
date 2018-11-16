bafdd5044de4a79581506d9260ad6c4e6587cef6
Andrii Boiko <andrij.boiko@gmail.com>
2018-11-16 17:07:22 +0200

    Revert "Revert "add Azure AD Auth""
    This reverts commit feeea49f3ff3e91f840f42110555424b9ac5d657.

---
+++ AzureAppExapmle/Config.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AzureAppExapmle
+{
+    public static class Config
+    {
+        public static string connString = "DefaultEndpointsProtocol=https;AccountName=storageexample2018;AccountKey=xEdafanGJA5ALwuhiyE8CgeOvs+3RboILDIJGXItRiKdIXxEaCeNzK12dQQGSteNFpxF3imDUBETaCwXh9yKbw==;EndpointSuffix=core.windows.net";
+    }
+}
